d089fe731b34caddb6bdc3d726b65524024bd5ab
ahmed-jawad828 <79979258+ahmed-jawad828@users.noreply.github.com>
2022-03-07 22:20:52 +0500

    Static Grids added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import {  useState } from 'react';
 import './App.css';
+import PuzzleGrids from './components/PuzzleGrids';
+// import puzzleGrids from './components/PuzzleGrids';
 
-function App() {
+const App = () => {
+
+  const [numofGrids, setNumofGrids] = useState(0);
+  // const [grids, setNumofGrids] = useState(0);
+  // console.log("@jawad ~ file: App.js ~ line 7 ~ numofGrids", numofGrids)
+
+  var a1 = [];
+  for (var i = 0; i < (numofGrids * numofGrids); i++) { a1.push(i + 1); }
+  var a2 = [];
+  while (a1.length) {
+    var pos = Math.random() * a1.length;
+    var element = a1.splice(pos, 1)[0];
+    a2.push(element);
+  }
+  console.log({ a2 })
+  // useEffect(() => {
+
+  // }, [numofGrids])
+  // const UserGreeting = ({ numofGrids }) => {
+  //   return <h1 className='text-4xl'>{numofGrids * numofGrids}</h1>;
+  // }
+
+
+  // if (numofGrids > 0) {
+  //   return <UserGreeting numofGrids={numofGrids} />;
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="css-selector h-full min-h-screen">
+      <div className='flex justify-center flex-col items-center '>
+        <h1 className='text-4xl font-bold font-sans text-black text-center pt-10'>
+          Grid Puzzle Task
+        </h1>
+        <div className='w-2/6 py-10'>
+          <label htmlFor="email" className="sr-only">
+            Email
+          </label>
+          <input
+            type="number"
+            name="number"
+            id="mumber"
+            className="shadow-sm  block w-full sm:text-lg outline-none border-gray-300 rounded-md h-10 px-5"
+            placeholder="Enter a Number from 1 - 10"
+            onChange={(e) =>
+              setNumofGrids(e.target.value)
+            }
+          />
+        </div>
+        {/* <div className="">
+          <button
+            type="button"
+            className="inline-flex items-center px-4 py-2 border border-transparent text-lg font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Submit
+          </button>
+        </div> */}
+
+
+      </div>
+
+
+      <div className={numofGrids>0 ? "":"hidden"}>
+            <PuzzleGrids numofGrids={numofGrids} a2={a2}/>
+
+
+      </div>
     </div>
   );
 }
---
+++ src/components/PuzzleGrids.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react'
+
+const PuzzleGrids = ({ numofGrids, a2 }) => {
+// console.log({expert:a2})
+
+const imgBox = document.querySelector('.dragStart');
+// console.log("@jawad ~ file: PuzzleGrids.jsx ~ line 7 ~ imgBox", imgBox)
+
+// const whiteBoxes = document.getElementsByClassName('dragStart');
+// console.log("@jawad ~ file: PuzzleGrids.jsx ~ line 9 ~ whiteBoxes", whiteBoxes)
+// whiteBoxes.addEventListener('dragstart', () => {
+//          console.log('DragStart has been triggered');
+//   //       // e.target.className += ' hold';
+//   //       // setTimeout(() => {
+//   //       //     e.target.className = 'hide';
+//   //       // }, 0);
+    
+//      });  
+
+// useEffect(() => {
+  //   whiteBoxes.addEventListener('dragstart', () => {
+  //       console.log('DragStart has been triggered');
+  //       // e.target.className += ' hold';
+  //       // setTimeout(() => {
+  //       //     e.target.className = 'hide';
+  //       // }, 0);
+    
+  //   });  
+  //   whiteBoxes.addEventListener('dragend', () => {
+  //       console.log('DragEnd has been triggered');
+  //       // e.target.className = 'imgBox';
+  //   });
+  // })
+  // useEffect(() => {
+  //   document.addEventListener("mousedown", handleClickOutside);
+  //   return () =>
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  // });
+// const whiteBoxes = document.getElementsByClassName('whiteBox');
+
+// Event listeners for draggable element imgBox
+
+
+  return (
+    <div className='flex flex-col items-center justify-center mt-4 w-full h-full' >
+
+      <div className='text-6xl font-extrabold text-center py-2 rounded-lg w-1/12 bg-green-600 text-white '>
+        {numofGrids * numofGrids}
+      </div>
+      <div className='mt-6 flex  gap-x-2  flex-wrap justify-center w-96' 
+      style={{height:"50vh"}} 
+      key="grid-id">
+
+      {a2?.map((item, index) => 
+                <button key={index} draggable="true" 
+                onDragEnter={(e)=> console.log(e)
+                }
+                className='dragStart text-2xl transform hover:bg-purple-900 transition duration-500 hover:scale-105 font-bold h-24 w-24 bg-purple-600 border-2 border-purple-900 my-1  rounded-lg text-white'>{item}</button>
+            )}
+      </div>
+    </div>
+  )
+}
+
+export default PuzzleGrids
